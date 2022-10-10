e88a448a39b7eaad79f906a16e6e2656daac63cc
captain-milous <milda.tesar3@gmail.com>
2022-10-10 08:12:17 +0200

    Main

--- TestDedicnost/Program.cs
+++ TestDedicnost/Program.cs
@@ -4,7 +4,14 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Test B, autor Miloš Tesař C3b");
+            Auto a1 = new Auto(45, 23);
+            NakladniAuto a2 = new NakladniAuto(80, 40, 12);
+            a1.NalijPalivo(3);
+            Majitel m1 = new Majitel("Jan", "Novak", a1);
+            Majitel m2 = new Majitel("Karel", "Sykora", a2);
+            Console.WriteLine(m1);
+            Console.WriteLine(m2);
         }
     }
 }
